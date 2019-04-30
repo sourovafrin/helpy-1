3a92c1dc3de7ef0dbaa7b61998d9c1c56f6dc542
sectionshankar <38269146+sectionshankar@users.noreply.github.com>
2019-04-30 10:48:27 +0600

    Update bot.py
    Chnage in time

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ SV=os.environ.get('SV')
 
 wls = Steem(node=['wss://wls.kidw.space/', 'https://wls.kidw.space/', 'https://wls.kennybll.com'])
 blockchain = Blockchain(steem_instance=wls, mode='head')
-whitelist = ['anritco', 'gabeboy','samest','djlethalskillz','adsactly','karinxxl','stackin','milkbox.esp','derangedvisions','tedtv']
+whitelist = ['anritco', 'gabeboy','samest','djlethalskillz','adsactly','karinxxl','stackin','milkbox.esp','derangedvisions','tedtv','dangyver']
 print("Running")
 for data in blockchain.stream('comment'):
     time.sleep(1)
@@ -25,9 +25,9 @@ for data in blockchain.stream('comment'):
             wls = Steem(node=['wss://wls.kidw.space/', 'https://wls.kidw.space/', 'https://wls.kennybll.com'],
                         keys=[SV])
             asyncio.sleep(1)
-            time.sleep(1500)
+            time.sleep(900)
             try:
-                post.upvote(weight=90,voter='sourov')
+                post.upvote(weight=80,voter='sourov')
                 post_age = post.time_elapsed()
             except Exception as e:
                 print(e)
