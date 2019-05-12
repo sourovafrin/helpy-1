b57e736025e176e04a27a18e34d4e1ef43be01e4
sourov <38147368+sourovafrin@users.noreply.github.com>
2019-05-12 20:45:43 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ SV=os.environ.get('SV')
 
 wls = Steem(node=['wss://wls.kidw.space/', 'https://wls.kidw.space/', 'https://wls.kennybll.com'])
 blockchain = Blockchain(steem_instance=wls, mode='head')
-whitelist = ['anritco','samest','karinxxl','stackin','thebugiq','derangedvisions','zakaria','tedtv','dangyver','ladyfont','azizbd','muh543','chilix']
+whitelist = ['anritco','samest','karinxxl','stackin','thebugiq','derangedvisions','zakaria','tedtv','dangyver','ladyfont','azizbd','muh543','chilix','sardart']
 print("Running")
 for data in blockchain.stream('comment'):
     time.sleep(1)
