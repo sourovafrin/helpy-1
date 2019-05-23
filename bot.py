7dfca4237a66e396437fd5d68971bd5507a5f797
sectionshankar <38269146+sectionshankar@users.noreply.github.com>
2019-05-24 03:36:01 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,32 +2,34 @@ from beem.steem import Steem
 from beem.blockchain import Blockchain
 from beem.comment import Comment
 from beem.utils import construct_authorperm
+from pymongo import MongoClient
+from datetime import timedelta
 import time
-import asyncio
 import os
 
-CH=os.environ.get('CH')
-SV=os.environ.get('SV')
+CH = os.environ.get('CH')
+SV = os.environ.get('SV')
+MD = os.environ.get('MD')
+
+client = MongoClient(MD)
+db = client.get_database("wls_db")
+record = db.wls_link
 
 wls = Steem(node=['wss://wls.kidw.space/', 'https://wls.kidw.space/', 'https://wls.kennybll.com'])
 blockchain = Blockchain(steem_instance=wls, mode='head')
-whitelist = ['anritco','samest','karinxxl','stackin','gaeljosser', 'thebugiq','zakaria','tedtv','dangyver','ladyfont','azizbd','muh543','chilix','sardart','xawi','rehan12']
+whitelist = ['anritco','samest','karinxxl','stackin','thebugiq','zakaria','tedtv','dangyver','ladyfont','azizbd','muh543','chilix','sardart','xawi','rehan12']
 cmnt = ['thebugiq']
+thresold = timedelta(minutes=29, seconds=30)
 print("Running")
-for data in blockchain.stream('comment'):
-    author = data['author']
-    perm = data['permlink']
-    permlink = construct_authorperm(author, perm)
-    post = Comment(permlink, steem_instance=wls)
-    if post.is_comment() == True:
-        pass
-    else:
-        if author in whitelist:
-            print("Queue: --{}--".format(author))
-            wls = Steem(node=['wss://wls.kidw.space/', 'https://wls.kidw.space/', 'https://wls.kennybll.com'], keys=[CH,SV])
-            asyncio.sleep(1)
-            time.sleep(1790)
-            print(post)
+
+
+def check():
+    for i in record.find():
+        perms = i['link']
+        post = Comment(perms, steem_instance=wls)
+        age = post.time_elapsed()
+        if age > thresold:
+            wls = Steem(node=['wss://wls.kidw.space/', 'https://wls.kidw.space/', 'https://wls.kennybll.com'], keys=[CH, SV])
             post.clear_cache()
             post.refresh()
             reward = float(post.reward)
@@ -41,15 +43,31 @@ for data in blockchain.stream('comment'):
             elif reward <= 0.4:
                 wt = 15
             else:
-                wt =10
+                wt = 10
             try:
-                post.upvote(weight=wt,voter='sourov')
-                post_age = post.time_elapsed()
-                post.upvote(weight=60,voter='mrcheisen')
+                post.upvote(weight=60, voter='mrcheisen')
+                time.sleep(1)
+                post.upvote(weight=wt, voter='sourov')
                 if author not in cmnt:
-                    post.reply("Ahoi, Your post has been upvoted by me and `@sourov`. Keep up the good work ✌\nReply `@sourov stop` in case you don't want comment anymore.",author="mrcheisen")
+                    post.reply("Ahoi, Your post has been upvoted by me and `@sourov`. Keep up the good work ✌\nReply `@sourov stop` in case you don't want comment anymore.", author="mrcheisen")
+                print("Upvoted {}\nTime elapsed {}\n\n*************************".format(perms, age))
+                record.delete_one({"link": perms})
             except Exception as e:
                 print(e)
-            print("Upvoted {}\nTime elapsed {}\n\n*************************".format(permlink,post_age))
         else:
-            pass
+            break
+
+
+for data in blockchain.stream('comment'):
+    if int(record.count_documents({})) > 0:
+        check()
+    else:
+        time.sleep(1)
+    author = data['author']
+    perm = data['permlink']
+    permlink = construct_authorperm(author, perm)
+    post = Comment(permlink, steem_instance=wls)
+
+    if post.is_comment() == False and author in whitelist:
+        link = {"link": permlink}
+        record.insert_one(link)
