2c252ba9a168454fe9ded54b1089aa211f36058a
sectionshankar <38269146+sectionshankar@users.noreply.github.com>
2019-05-31 10:51:27 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -27,6 +27,47 @@ whitelist = ['anritco','samest','karinxxl', 'heyimsnuffles', 'chrisrendon', 'the
 cmnt = ['thebugiq', 'haejin', 'backpackingmonk', 'marinella', 'al-desnudo', 'sardart']
 special = ['anritco', 'heyimsnuffles', 'marinella', 'joseph1956', 'thebugiq', 'ladyfont', 'muh543', 'haejin', 'backpackingmonk']
 
+"""
+alric = 16
+infarno = 5
+zinter = 49
+lyanna = 27
+tyrus = 38
+talia = 70
+jarlax = 74
+seachan = 71
+foxwood = 72
+kiara = 73
+plado = 110
+Valnamor = 111
+Rennyn = 112
+Peakrider = 113
+Mancer = 109
+Selenia = 56
+62: Elven Cutthroat
+64: Cocatrice
+66: Enchanted Pixie
+68: Magi Sphinx
+9: Fire Demon
+10: Serpent of the Flame
+11: Elemental Phoenix
+20: Mischievous Mermaid
+21: Naga Warrior
+22: Frost Giant
+32: Swamp Thing
+33: Spirit of the Forest
+42: Defender of Truth
+43: Air Elemental
+44: Angel of Light
+53: Dark Enchantress
+54: Screaming Banshee
+55: Lord of Darkness
+57: Lightning Dragon
+58: Chromatic Dragon
+59: Gold Dragon
+"""
+
+
 di = {'16': .55,
        '5': .42,
       '49': .45,
@@ -36,7 +77,28 @@ di = {'16': .55,
       '71': .45,
       '72': .55,
       '73': .45,
-      '74': .4,}
+      '74': .4,
+      '62': .09,
+      '64': .10,
+      '66': .1,
+      '68': 1,
+      '9': .3,
+      '10': .3,
+      '11': 1.5,
+      '20': .50,
+      '21': .25,
+      '22': 1.2,
+      '32': .35,
+      '33': 2.5,
+      '42': .3,
+      '43': .5,
+      '44': 1.2,
+      '53': .25,
+      '54': .25,
+      '55': 1.5,
+      '57': 1.5,
+      '58': 1.5,
+      '59': 1.5}
 
 
 dic = {'16': .7,
