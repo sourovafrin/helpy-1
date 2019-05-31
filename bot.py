66188ea3e515954fb8e0c3a947d807c9e4786a4a
sectionshankar <38269146+sectionshankar@users.noreply.github.com>
2019-06-01 01:13:10 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -424,7 +424,7 @@ def st():
                             if str(each['card_detail_id']) == card_number and each['gold'] == is_gold and int(each['edition']) == int(edit):
                                 second_min = float(each['low_price'])
                         percent = round(100 - (card_price / second_min * 100), 3)
-                        per = 15
+                        per = 0
                         if percent > per:
                             sbd_price = requests.get("https://steemmonsters.com/purchases/settings").json()['sbd_price']
                             sbd_send = round(card_price / sbd_price, 3)
