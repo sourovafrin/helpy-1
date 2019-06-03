62c2cf2561a48b506a1d2ffc91f690056410ac62
sectionshankar <38269146+sectionshankar@users.noreply.github.com>
2019-06-03 12:50:36 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 from beem.steem import Steem
 from beem.account import Account
-from beem.nodelist import NodeList
+#from beem.nodelist import NodeList
 from beem.blockchain import Blockchain
 from beem.comment import Comment
 from beem.utils import construct_authorperm
@@ -417,7 +417,7 @@ def st():
     
     nodelist = NodeList()
     nodelist.update_nodes()
-    stm = Steem(node= nodelist.get_nodes())
+    stm = Steem(node= 'https://api.steemit.com')
     chain = Blockchain(stm, "head")
     print("started sm")
     for detail in chain.stream(['custom_json']):
